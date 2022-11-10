bc4d0ea6d4597c791daff50d0cd8d9075474f29e
Amir Abroun <abroon234@gmail.com>
2022-11-11 00:52:34 +0330

    hello world :)

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
 <?php require __DIR__ . '/vendor/autoload.php';
 
-use App\Models\User;
-
-User::query()->adduser(
-    [
-        'name' => 'Sajjad',
-        'last_name' => 'Vosough',
-        'age' => 20,
-        'gender' => 'male',
-        'phone' => '0987654321',
-        'email' => '****@gmail.com'
-    ]
-)->adduser(
-    [
-        'name' => 'Amir',
-        'last_name' => 'Abron',
-        'age' => 20,
-        'gender' => 'male',
-        'phone' => '0987654321',
-        'email' => '****@gmail.com'
-    ]
-)->adduser(
-    [
-        'name' => 'Arthur',
-        'last_name' => 'Morgan',
-        'age' => 24,
-        'gender' => 'male',
-        'phone' => '0987654321',
-        'email' => '****@gmail.com'
-    ]
-)->saveUsers();
-
-dd(User::query()->users());
+dd("hello world");
